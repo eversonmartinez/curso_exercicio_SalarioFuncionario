6354e9bfdc2a33242bc4dfb8ed4f3a1cd5b9655a
Everson Martinez <118080010+eversonmartinez@users.noreply.github.com>
2023-02-08 16:37:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# curso_exercicio_SalarioFuncionario
+ Exercício simples de um curso de Java para calcular o salário líquido atual e um salário com aumento.
---
+++ src/entities/Employee.java
@@ -0,0 +1,19 @@
+package entities;
+
+public class Employee {
+	public String name;
+	public double grossSalary;
+	public double tax;
+	
+	public double netSalary() {
+		return (this.grossSalary - this.tax);
+	}
+	
+	public void increaseSalary(double percentage) {
+		this.grossSalary+=(this.grossSalary*(percentage/100)-this.tax);
+	}
+	
+	public String toString() {
+		return (this.name + ", $" + this.grossSalary);
+	}
+}
